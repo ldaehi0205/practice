ea968a3fec73c16a85725c5ed3fc660124d294f3
이대희 <leedaehee@idaehuiui-MacBookPro.local>
2021-06-07 01:52:34 +0900

    ADD axios

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, { useState } from "react";
 import { Navbar, Container, NavDropdown, Nav } from "react-bootstrap";
 import Data from "./data";
+import Detial from "./Detail";
 import styled from "styled-components";
+import { Link, Route } from "react-router-dom";
+import axios from "axios";
 
 function App() {
   const [sheos, setsheos] = useState(Data);
@@ -10,12 +13,12 @@ function App() {
     <>
       <Navbar bg="light" expand="lg">
         <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Brand href="/">React-Bootstrap</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/detail">Detail</Nav.Link>
               <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.2">
@@ -33,13 +36,38 @@ function App() {
           </Navbar.Collapse>
         </Container>
       </Navbar>
-      <div className="container">
-        <div className="row">
-          {sheos.map(e => {
-            return <Card key={e.id} sheos={e} />;
-          })}
+
+      <Route exact path="/">
+        <div className="container">
+          <div className="row">
+            {sheos.map(e => {
+              const asd = `/detail/${e.id}`;
+              return (
+                <Link to={asd}>
+                  <Card key={e.id} sheos={e} />
+                </Link>
+              );
+            })}
+          </div>
+          <button
+            onClick={() => {
+              axios
+                .get("https://codingapple1.github.io/shop/data2.json")
+                .then(result => {
+                  //result는 성공했을때 정보를 갖고잇음
+                  const aaa = [...sheos, ...result.data];
+                  setsheos(aaa);
+                });
+            }}
+          >
+            더보기
+          </button>
         </div>
-      </div>
+      </Route>
+
+      <Route exact path="/detail/:id">
+        <Detial sheos={sheos} />
+      </Route>
     </>
   );
 }
---
+++ src/Detail.js
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from "react";
+import { useHistory, useParams } from "react-router-dom";
+
+function Detail(props) {
+  const history = useHistory();
+  const { id } = useParams();
+  const [alert, setalert] = useState(true);
+  const [inputData, setinputData] = useState("");
+
+  useEffect(() => {
+    let timer = setTimeout(() => {
+      setalert(false);
+    }, 2000);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
+
+  // const product = props.sheos.find(() => {
+  //   {
+  //     return product.id === id;
+  //   }
+  // });
+
+  return (
+    <div className="container">
+      <div>
+        <header>Detail</header>
+      </div>
+
+      <input
+        onChange={e => {
+          setinputData(e.target.value);
+        }}
+      />
+
+      {alert === true && <div>재고가 얼마 남지 않았습니다.</div>}
+      <div className="row">
+        <div className="col-md-6">
+          <img
+            src="https://codingapple1.github.io/shop/shoes1.jpg"
+            width="100%"
+          />
+        </div>
+        <div className="col-md-6 mt-4">
+          <h4 className="pt-5">{props.sheos[id].title}</h4>
+          <p>{props.sheos[id].content}</p>
+          <p>{props.sheos[id].price}</p>
+          <button className="btn btn-danger">주문하기</button>
+          <button
+            className="btn btn-danger"
+            onClick={() => {
+              history.goBack();
+            }}
+          >
+            뒤로가기
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Detail;
